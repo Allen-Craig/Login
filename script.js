037fc1435faf56b49cec9667bd6c55a52c865a5c
Allen Craig <TheMightyCraig@gmail.com>
2022-02-06 13:55:48 -0700

    updated number of users and passwords

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
+//
 const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-form-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
 
-loginButton.addEventListener("click", (c) => {
-    c.preventDefault();
+loginButton.addEventListener("click", (x) => {
+    x.preventDefault();
     const username = loginForm.username.value;
     const password = loginForm.password.value;
 
-    if (username === "Allen" && password === "password1") {
+    
+    
+    
+    
+    if (username === "User1" && password === "password1") {
         alert("You have successfully logged in.");
         location.reload();
-    } else {
+    } if (username === "User2" && password === "password2") {
+        alert("You have successfully logged in.");
+        location.reload();
+    } if (username === "User3" && password === "password3") {
+        alert("You have successfully logged in.");
+        location.reload();
+    } if (username === "Admin" && password === "AdminPass99") {
+        alert("You have successfully logged in.");
+        location.reload();
+    } 
+    else {
         loginErrorMsg.style.opacity = 1;
     }
 })
